60b985d95a795e98d7d1df7de4e19112c000a92b
Raphael <ugwuraphael@gmail.com>
2017-05-30 19:45:33 +0100

    Add files via upload

--- weather-app.js
+++ weather-app.js
@@ -1,36 +1,104 @@
-var Geo = {};
-if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(success,error);
-}
-else {
-    alert('Geolocation is not supported');
+var APPID = "0f692dea59473fc56b118cce9d5fb334";
+var temp;
+var loc;
+var icon;
+var humidity;
+var wind;
+var direction;
+
+function updateByZip(zip) {
+    var url = "http://api.openweathermap.org/data/2.5/weather?" + 
+    "zip=" + zip +
+    "&APPID=" + APPID;
+    sendRequest(url);
 }
 
-function error() {
-    alert("That's weird! We couldn't find you!");
+function updateByGeo(lat, lon) {
+    var url = "http://api.openweathermap.org/data/2.5/weather?" +
+        "lat=" + lat +
+        "&lon=" + lon +
+        "&APPID=" + APPID +
+    sendRequest(url);
+ }
+
+function sendRequest(url) {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            var data = JSON.parse(xmlhttp.responseText);
+            var weather = {};
+            weather.icon = data.weather[0].main;
+            weather.humidity = data.main.humidity;
+            weather.wind = data.wind.speed;
+            weather.direction = degreesToDirection(data.wind.deg);
+            weather.loc = data.name;
+            weather.temp = kelvintoCelsius(data.main.temp);
+            update(weather);
+        }
+    };
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
 }
 
-function success(position) {
-    Geo.lat = position.coords.latitude;
-    Geo.lng = position.coords.longitude;
-}
+function degreesToDirection(degrees) {
+    var range = 360/16;
+    var low = 360 - range/2;
+    var high = (low + range) % 360;
+    var angles = ["N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW"]
+    for (i in angles) {
 
-var key = "f672ff13193bfcc40427a678ebfdbc71";
-var weather = "http://api.wunderground.com/api/”+ key +”/forecast/geolookup/conditions/q/" + Geo.lat + "," + Geo.lng + ".json";
-$.ajax({
-    url : weather,
-    dataType : "jsonp",
-    success : function(data) {
-        var location =data['location']['city'];
-        var temp = data['current_observation']['temp_f'];
-        var img = data['current_observation']['icon_url'];
-        var desc = data['current_observation']['weather'];
-        var wind = data['current_observation']['wind_string'];
+        if(degrees>= low && degrees < high)
+            return angles[i];
+
+        low = (low + range) % 360;
+        high = (high + range) % 360;
     }
-})
 
-$('#location').html(location);
-$('#temp').html(temp);
-$('#desc').html(desc);
-$('#wind').html(wind);
-$('#img').attr('src', img);
+}
+
+function kelvintoCelsius(k) {
+    return Math.round(k -273.15);
+};
+
+function kelvintoFarenheit() {
+    return Math.round(k * (9/5) - 459.67);
+};
+
+var wImages = {
+    Clouds: 'clouds.png',
+    Rainy: 'rainy.png',
+    Clear: 'clear.png'
+}
+
+function update(weather) {
+    wind.innerHTML = weather.wind;
+    direction.innerHTML = weather.direction;
+    humidity.innerHTML = weather.humidity + " %";
+    temp.innerHTML = weather.temp;
+    icon.src = "imgs/" + wImages[weather.icon];
+    loc.innerHTML = weather.loc;
+}
+
+function showPosition(position) {
+    updateByGeo(position.coords.latitude, position.coords.longitude);
+}
+
+
+
+window.onload = function() {
+    temp = document.getElementById("temperature");
+    loc = document.getElementById("location");
+    icon = document.getElementById("icon");
+    humidity = document.getElementById("humidity");
+    wind = document.getElementById("wind");
+    direction = document.getElementById("direction");
+
+    if(navigator.geolocation){
+       navigator.geolocation.getCurrentPosition(showPosition);
+
+   } else {
+       var zip = window.prompt("Could not discover your location. What is your zip code?");
+       updateByZip(zip);
+   }
+ }
+
